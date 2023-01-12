05fd353f2c50330f5eda749dc257573d55074963
kdxcxs <cx@kdxcxs.com>
2023-01-12 17:42:34 +0800

    fix: 添加方向介绍页面遗漏部分

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script>
   import Header from './components/Header.svelte';
   import Intro from './components/Intro.svelte';
+  import Field from './components/Field.svelte';
 </script>
 
 <main class="w-full">
   <Header />
   <div class="content">
     <Intro />
+    <Field />
   </div>
 </main>
 
